f978ebc10c18aa0f55c0414bf3c27d26594a1c20
nunjachomp <nunjachomp@gmail.com>
2023-07-10 23:23:05 +0300

    Nascent Menu 1

--- js/dropdowns.js
+++ js/dropdowns.js
@@ -4,6 +4,8 @@ loveGodButton.addEventListener("click", main);
 const loveNeighborButton = document.querySelector(".section2");
 loveNeighborButton.addEventListener("click", main2);
 
+const mitzvah1 = document.querySelector(".mitzvah1");
+
 async function getVerses(){
     let response = await fetch("https://649e6352245f077f3e9c56d0.mockapi.io/api/v1/calculation");
     let data = await response.json();
@@ -32,7 +34,18 @@ async function main2() {
     }
   }
 
+  async function mitzvahOne() {
+    try {
+      let abc = await getVerses();
+      mitzvah1.innerHTML = `${abc[4].text}`;
+      console.log(abc[4].text)
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
+
+//-------------------------------------------------------------
   /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
@@ -52,3 +65,6 @@ function myFunction() {
       }
     }
   }
+  //------------------------------------------------------------
+
+  
