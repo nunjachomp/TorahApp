1cd9ede3c2def33bf64b7cee15cf90990dd6ed36
nunjachomp <nunjachomp@gmail.com>
2023-07-11 15:20:57 +0300

    Button Layouts Adjusted

--- js/dropdowns.js
+++ js/dropdowns.js
@@ -65,6 +65,24 @@ function myFunction() {
       }
     }
   }
-  //------------------------------------------------------------
+  //
+  function myFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.section2')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content2");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show2')) {
+          openDropdown.classList.remove('show2');
+        }
+      }
+    }
+  }
+  //----------------------------------------------------------
 
   
